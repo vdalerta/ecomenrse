392a5a56d32a022d2cdc977940f3d0ffc8cab404
raycogarcia13 <42067571+raycogarcia13@users.noreply.github.com>
2021-10-23 21:25:51 -0400

    quitando node_moudles del repositorio, arreglado el gitignore y duncioandno tu proyecto

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-const express = require 
+const express = require("express") 
 const app = express ();
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
+
 const app = require ('./app')
-
-
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config({path:'backend/config/config.env'});
 
 
 
 
 app.listen(process.env.PORT,()=> {
-    console.log(`server started on PORT: ${process.env.PORT}in ${process.env.NODE_ENV}mode.`)
+    console.log(`server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
 })
